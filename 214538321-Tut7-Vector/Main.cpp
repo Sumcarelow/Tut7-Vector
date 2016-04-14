2f4ea624355ad0b36d23bb2fd7cd053f41010d0d
samkelo <sumcarelow@gmail.com>
2016-04-14 15:48:12 +0200

    final commit

--- 214538321-Tut7-Vector/Main.cpp
+++ 214538321-Tut7-Vector/Main.cpp
@@ -7,24 +7,25 @@ using namespace std;
 int main(){
 	vector<int> myVector;
 	vector<int> ::iterator Sam;
+	vector<int>::iterator nto;
 
 	for (int i = 0; i <= 19; i++)
 	{
 		myVector.push_back(rand() % 100 + 1);
 	}
 
-	for (int c = 0; c <= 19; c++)
+	for (Sam = myVector.begin(); Sam <= myVector.end(); Sam++)
 	{
-		cout << myVector[c] << endl;
+		cout << Sam << endl;
 	}
 
 	cout << endl;
 
 	sort(myVector.begin(), myVector.end());
 
-	for (int c = 0; c <= 19; c++)
+	for (nto = myVector.begin(); nto <= myVector.end(); nto++)
 	{
-		cout << myVector[c] << endl;
+		cout << nto << endl;
 	}
 
 	system("pause");
