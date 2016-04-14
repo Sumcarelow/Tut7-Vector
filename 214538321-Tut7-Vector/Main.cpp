f01ba7da76268fe2d3387d009a0a424cba1dee28
samkelo <sumcarelow@gmail.com>
2016-04-14 15:40:35 +0200

    sorted the vector using sort algorithm

--- 214538321-Tut7-Vector/Main.cpp
+++ 214538321-Tut7-Vector/Main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -7,15 +8,25 @@ int main(){
 	vector<int> myVector;
 	vector<int> ::iterator Sam;
 
-	for (int i = 0; i <= 20; i++)
+	for (int i = 0; i <= 19; i++)
 	{
 		myVector.push_back(rand() % 100 + 1);
 	}
 
-	for (int c = 0; c <= 20; c++)
+	for (int c = 0; c <= 19; c++)
 	{
 		cout << myVector[c] << endl;
 	}
+
+	cout << endl;
+
+	sort(myVector.begin(), myVector.end());
+
+	for (int c = 0; c <= 19; c++)
+	{
+		cout << myVector[c] << endl;
+	}
+
 	system("pause");
 	return 0;
 
